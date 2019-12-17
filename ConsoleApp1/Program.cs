6110498c198ec03af779f94d56da2e4916f1c8c4
JEFF ECKMAN <netspy03@msn.com>
2019-12-16 20:15:26 -0700

    Added the C# start game

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Item Game");
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome {0}, are you ready to begin?", userName);
+            Console.WriteLine("Hit any ket to continue...");
+            Console.ReadKey();
+
+        }
+    }
+}
